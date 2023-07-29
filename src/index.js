d4ba617283eae3a9d35bb25edc57d56e92b591b2
pavlo <work.reiste@gmail.com>
2023-07-29 20:16:41 +0300

    header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './sass/main.scss';
-import mylogo from './assets/flaticon-code-lapetop.png';
+// import mylogo from './assets/flaticon-code-lapetop.png';
 
-const mainImage = document.getElementById("mainImage")
-mainImage.src = mylogo
+// const mainImage = document.getElementById("mainImage")
+// mainImage.src = mylogo
 
 const hello = require('./js/hello');
+
+// бургер меню 
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu, .header__burger-span').toggleClass('active');
+        $('body').toggleClass('block');
+    });
+});
+
+// кінець бургер меню 
--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,17 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource'
-            }
+            },
         ]
     },
     plugins: [
