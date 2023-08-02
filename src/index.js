3ba57d63e27f61ae2ea109764b3aa8240305a1c4
pavlo <osnova.reiste@gmail.com>
2023-08-02 19:03:54 +0300

    work cowo wensday

--- src/index.js
+++ src/index.js
@@ -16,3 +16,56 @@ $(document).ready(function() {
 });
 
 // кінець бургер меню 
+
+new Swiper('.image-slider', {
+    // arrows 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    // навігація
+    pagination: {
+        el: '.swiper-pagination',
+        // буллети
+        clickable: true,
+        // буллети динамічні
+        dynamicBullets: true,
+        // буллети кастомні
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+        // // фракція
+        // type: 'fraction',
+        // // фракція кастомна
+        // renderFraction: function(currentClass, totalClass) {
+        // return ' Фото <span class="' + currentClass + '"></span>' +
+        //     ' з ' +
+        //     '<span class="' + totalClass + '"></span>';
+        // // прогресбар
+        // type: 'progresbar', 
+        },
+    // кількість відображення картинок на одному слайді
+    slidesPerView: 3,
+    // // скролбар
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //     draggable: true
+    // },
+    // курсор перетаскування
+    grabCursor: true,
+    // відступ між слайдами
+    spaceBetween: 20,
+    // слайд мишкою
+    // mousewheel: {
+    //     sensitivity: 1,
+    // },
+    // // відступ між слайдами
+    // spaceBetween: 30,
+    // // кількість пролистуюмих слайдів
+    // slidePerGroup: 1,
+    // // активний слайд по центру
+    // centeredSlides: true,
+});
+
+
+// ініціалізація слайдера 
